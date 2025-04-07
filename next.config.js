b101d6ade50cac5b73a9c8963dc6709dd704f8ae
Nody Peng <ablecck12@gmail.com>
2025-04-07 18:53:19 +0800

    modify

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  output: "export", // This setting ensures Next.js outputs static HTML files
+  distDir: "out", // Specifies the build output directory
+  // Add any other needed configuration
 };
 
 module.exports = nextConfig;
