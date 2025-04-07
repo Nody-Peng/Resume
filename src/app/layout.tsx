91eec52ace4aafd48a6f3bc4cc29816911311274
Nody Peng <ablecck12@gmail.com>
2025-04-07 22:40:41 +0800

    doc: modify the title.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Resume_Peng-Tsung-Chun",
+  title: "Peng-Tsung-Chun",
   description: "Profile_Peng, Tsung-Chun",
 };
 
